6e080744b2e5e5e2e165fd747f84062202a41151
Carlos André <andre.tjlba@gmail.com>
2022-04-26 18:09:43 -0300

    Estrutura de repetição While

---
+++ src/com/carlosandre/cursojava/aula16/LoopWhile.java
@@ -0,0 +1,15 @@
+package com.carlosandre.cursojava.aula16;
+
+public class LoopWhile {
+
+	public static void main(String[] args) {
+		int contador = 0;
+		
+		while(contador < 10) {
+			System.out.println("Contador -> " + contador);
+			contador++;
+		}
+
+	}
+
+}
