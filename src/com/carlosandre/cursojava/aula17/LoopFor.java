3ae8d945e7be7b0d53f777db833f0837a9690511
Carlos André <andre.tjlba@gmail.com>
2022-04-26 18:10:59 -0300

    Estrutura de repetição For

---
+++ src/com/carlosandre/cursojava/aula17/LoopFor.java
@@ -0,0 +1,14 @@
+package com.carlosandre.cursojava.aula17;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+		int fim = 20;
+		
+		for(int i = 1; i <= fim; i++) {
+			System.out.println("-> " + i);
+		}
+
+	}
+
+}
